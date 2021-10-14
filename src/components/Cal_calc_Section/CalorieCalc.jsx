f4058dc76d4cab2babb9155f0bb93bd81aad1884
Jaswanth <pallavolujaswanth2019@iiitkottayam.ac.in>
2021-10-14 10:56:30 +0530

    calc_sec-oct-14-1

--- src/components/Cal_calc_Section/CalorieCalc.jsx
+++ src/components/Cal_calc_Section/CalorieCalc.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 
+import axios from 'axios'
+
 export default function CalorieCalc() {
+    const fetchData = async () => {
+        var options = {
+            method: 'GET',
+            url: 'https://api.edamam.com/api/food-database/v2/parser',
+            params: {
+                app_id: 'f31fef05',
+                app_key: ' a4574338171629cdd3068e3d5be69484',
+                ingr: 'apple'
+            },
+        }
+        await axios.request(options)
+            .then(data => console.log(data.data))
+            .catch(err => console.log(err))
+    }
+
 
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
             <h1>this is cal-calc section</h1>
+            <button onClick={fetchData}>Fetch API</button>
         </div>
     )
 }
