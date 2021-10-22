8cab30949b53d63b213c62bff74a8eb7df34c810
Jaswanth <pallavolujaswanth2019@iiitkottayam.ac.in>
2021-10-22 12:52:30 +0530

    oct22-app.css

--- src/components/Cal_calc_Section/CalorieCalc.jsx
+++ src/components/Cal_calc_Section/CalorieCalc.jsx
@@ -1,28 +1,51 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import './cal_cal.css';
+import { useState } from 'react';
 
-import axios from 'axios'
-
-export default function CalorieCalc() {
-    const fetchData = async () => {
+function CalorieCalc() {
+    const [query, setquery] = useState("");
+    const [info, setInfo] = useState([]); //eslint-disable-line
+    const fetchData = async (e) => {
+        e.preventDefault()
         var options = {
             method: 'GET',
             url: 'https://api.edamam.com/api/food-database/v2/parser',
             params: {
                 app_id: 'f31fef05',
                 app_key: ' a4574338171629cdd3068e3d5be69484',
-                ingr: 'apple'
+                ingr: query
             },
         }
+
         await axios.request(options)
-            .then(data => console.log(data.data))
+            .then(result => setInfo(result.data.parsed))
             .catch(err => console.log(err))
+
     }
 
 
+
     return (
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <h1>this is cal-calc section</h1>
-            <button onClick={fetchData}>Fetch API</button>
+        <div className="calc_main">
+            <div className="calc">
+                <h1 onClick={fetchData}>Calorie Calculator 🧮</h1>
+
+                <div className="calc_searchform">
+                    <input
+                        type="text"
+                        className="calc_input"
+                        placeholder="Enter Ingredient"
+                        value={query}
+                        onChange={(e) => setquery(e.target.value)}
+                    />
+                    <button className="calc_submit" onClick={fetchData}>Search</button>
+                </div>
+            </div>
+
+            <div>
+            </div>
         </div>
-    )
+    );
 }
+export default CalorieCalc;
