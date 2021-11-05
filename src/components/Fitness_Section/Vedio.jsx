44317350623e171d581499bc262101893c76cfe8
sainruthik <71174369+sainruthik@users.noreply.github.com>
2021-11-05 18:12:14 +0530

    fitness_nov05

--- src/components/Fitness_Section/Vedio.jsx
+++ src/components/Fitness_Section/Vedio.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Vedio({ item }) {
     return (
         <div className="vedio">
-            <iframe width="400" height="250" src={item.link} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="400" height="250" src={item.link} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
         </div>
     )
